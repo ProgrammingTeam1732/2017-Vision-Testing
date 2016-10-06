e551665807bf45e19e606670ca4fe075ef21d639
JayShower <lacrossejason@gmail.com>
2016-10-05 20:21:08 -0500

    Added resolution settings

--- AxisCamera/src/testing/Testing.java
+++ AxisCamera/src/testing/Testing.java
@@ -7,9 +7,10 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 import camera.AxisCamera;
+import camera.AxisCamera.Resolution;
 
 public class Testing {
-	
+
 	public static void main(String[] args) throws Exception {
 		AxisCamera camera = new AxisCamera("169.254.148.78");
 		ImageIcon icon = new ImageIcon(camera.getBufferedImage());
@@ -19,9 +20,32 @@ public class Testing {
 		frame.pack();
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		while ( true ) {
+		Timer timer = new Timer();
+		while (timer.getElapsedTime() < 10 * 1000) {
 			icon.setImage(camera.getBufferedImage());
 			frame.repaint();
 		}
+		camera.setResolution(Resolution.r320x240);
+		timer.reset();
+		while (timer.getElapsedTime() < 10 * 1000) {
+			icon.setImage(camera.getBufferedImage());
+			frame.repaint();
+		}
+	}
+}
+
+class Timer {
+	private long time;
+
+	public Timer() {
+		time = System.currentTimeMillis();
+	}
+
+	public long getElapsedTime() {
+		return System.currentTimeMillis() - time;
+	}
+
+	public void reset() {
+		time = System.currentTimeMillis();
 	}
 }
